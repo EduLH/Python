f58fb53b7b704a77d1c6a2b0d3c744ce7995f8d8
EduLH <eduardo_lh@protonmail.com>
2018-02-26 16:13:11 -0300

    Modificado para padroes empresariais

--- troco.py
+++ troco.py
@@ -1,21 +1,25 @@
 resultado = []
+
 resultados = []
+
 def troco(caixa, goal):
     if goal < 0:
         return
+
     if goal == 0 and sorted(resultado) not in resultados:
         resultados.append(sorted(resultado))
+
     else:
         for i in caixa:
             resultado.append(i)
             troco(caixa, goal-i)
             resultado.pop()
-#
+
 goal = int(input())
 tamam = int(input())
-Zeca = []
+moedas = []
 for _ in range(tamam):
-    Zeca.append(int(input()))
-troco(Zeca, goal)
+    moedas.append(int(input()))
+troco(moedas, goal)
 
 print (len(resultados))
