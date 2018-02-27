a9f5b91fa03c907994c71a471d814b5ddf1f0bd7
EduLH <eduardo_lh@protonmail.com>
2018-02-27 13:06:29 -0300

    modificado para BUG de parenteses nao fechados

--- BalancParent.py
+++ BalancParent.py
@@ -6,8 +6,10 @@ for i in parenteses:
             limit = limit+1
         else:
             limit = limit-1
-    else:       #caso nao seja, mais parenteses estao sendo fechados q o necessario
-        print('false')  #ou houve 'fechamento' nesnecessario de parenteses
+    else: #caso nao seja, mais parenteses estao sendo fechados q o necessario
+          #ou houve 'fechamento' nesnecessario de parenteses
         break
-if limit >=0:   
+if limit ==0:  #tem de ser igual a 0 para garantir q todos os parenteses sejam fechados
     print('true')
+else:
+    print('false')
